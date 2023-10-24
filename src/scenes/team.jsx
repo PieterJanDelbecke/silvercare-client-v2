b3f14df842bbdab08ffd23d9412aee2859b60b48
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-10-24 11:37:36 +1100

    initial setup of table

--- src/scenes/team.jsx
+++ src/scenes/team.jsx
@@ -1,15 +1,75 @@
 import styled from "@emotion/styled";
-import { mockDataTeam } from "../data/mockData";
-import { useEffect } from "react";
+import { useState, useMemo } from "react";
+import {
+	flexRender,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	useReactTable,
+} from "@tanstack/react-table";
+import { DateTime } from "luxon";
+
+import residents from "../data/MOCK_DATA.json";
+import { PageHeading } from "../common/typography";
 
 const Container = styled.div`
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+	color: black;
+	/* background-color: orange; */
 `;
 const Team = () => {
-	console.log("DATA", mockDataTeam);
-	return <Container>Team2</Container>;
+	const data = useMemo(() => residents, []);
+
+	/** @type import('@tanstack/react-table').ColumnDef<any> */
+
+	const columns = [
+		{ header: "ID", accessorKey: "id" },
+		{ header: "First Name", accessorKey: "first_name" },
+		{ header: "Last Name", accessorKey: "last_name" },
+		{ header: "Email", accessorKey: "email" },
+		{ header: "Gender", accessorKey: "gender" },
+		{
+			header: "DOB",
+			accessorKey: "dob",
+			cell: (info) => DateTime.fromISO(info.getValue()).toLocaleString(DateTime.DATE_MED),
+		},
+	];
+
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
+	return (
+		<Container>
+			<PageHeading>Team</PageHeading>
+			<table>
+				<thead>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<th key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map((row) => (
+						<tr key={row.id}>
+							{row.getVisibleCells().map((cell) => (
+								<td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</Container>
+	);
 };
 
 export default Team;
