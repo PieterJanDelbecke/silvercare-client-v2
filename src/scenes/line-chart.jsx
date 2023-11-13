d67c0fe00f28c93026bf3f7dd1c3284098bae6f3
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-11-13 11:41:45 +1100

    setup Linechart

--- src/scenes/line-chart.jsx
+++ src/scenes/line-chart.jsx
@@ -4,12 +4,14 @@ import { PageHeading, Heading } from "../common/typography";
 
 import Context from "../context/context";
 import { colors } from "../styles/theme";
+import MyLineChart from "./components/LineChart";
 
 const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+	height: 75vh;
 `;
 
 const ResidentName = styled(Heading)`
@@ -26,7 +28,7 @@ const LineChart = () => {
 		<Container>
 			<PageHeading>Line Chart</PageHeading>
 			<ResidentName>{selectedResident?.firtName || "John Doe"}</ResidentName>
-			{/* <MyLineCart /> */}
+			<MyLineChart />
 		</Container>
 	);
 };
