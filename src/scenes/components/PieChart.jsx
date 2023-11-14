dfbccb8b91c57ed13332f9a4faa13cf08fd5dcd9
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-11-14 13:34:17 +1100

    charts + setup resident form expanded

--- src/scenes/components/PieChart.jsx
+++ src/scenes/components/PieChart.jsx
@@ -7,6 +7,7 @@ const MyResponsivePie = () => {
 	return (
 		<ResponsivePie
 			data={data}
+			animate={false}
 			margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
 			innerRadius={0.5}
 			padAngle={0.7}
