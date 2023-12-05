42a8fe406290f2db21832dab0679180e4c23a57f
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-12-05 11:01:03 +1100

    initial setup of common styling and activity new-form

---
+++ src/scenes/activity-new-form.jsx
@@ -0,0 +1,12 @@
+import { Container, InputsContainer } from "../common/styling";
+import { PageHeading } from "../common/typography";
+
+const ActivityNewForm = () => {
+	return (
+		<Container>
+			<PageHeading>New Activity Form</PageHeading>
+		</Container>
+	);
+};
+
+export default ActivityNewForm;
