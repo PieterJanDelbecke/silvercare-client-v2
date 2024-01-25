5fd8403f5cf8c02ff8612cc2a624da9b3b52374e
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2024-01-25 13:32:09 +1100

    PieChart component

--- src/scenes/pie-chart.jsx
+++ src/scenes/pie-chart.jsx
@@ -1,16 +1,39 @@
 import styled from "@emotion/styled";
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { PageHeading, Heading } from "../common/typography";
 import MyResponsivePie from "./components/PieChart";
 import Context from "../context/context";
 import { colors } from "../styles/theme";
+import api from "../api/api";
+
+import Select from "react-select";
+import { mockPieData } from "../data/mockData";
+
+import {
+	// Container,
+	InputsContainer,
+	Grid,
+	InputContainer,
+	InputLabel,
+	Error,
+	selectOptionStyles,
+	Input,
+	ButtonContainer,
+	SubmitButton,
+} from "../common/styling-forms";
 
 const Container = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
+	/* justify-content: center; */
 	align-items: center;
-	height: 75vh;
+	height: 60vh;
+`;
+
+const FormContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	width: 400px;
 `;
 
 const ResidentName = styled(Heading)`
@@ -19,13 +42,47 @@ const ResidentName = styled(Heading)`
 
 const PieChart = () => {
 	const { context, setContext } = useContext(Context);
-	const { selectedResident } = context;
+	const { residents } = context;
+	const [data, setData] = useState(mockPieData);
+	console.log("### residents", residents);
+
+	const residentsData = residents.map((resident) => {
+		return {
+			value: resident.id,
+			label: `${resident.firstName} ${resident.lastName}`,
+		};
+	});
+	const [selectedResident, setSelectedResident] = useState(residentsData[0]);
+
+	const viewResident = async () => {
+		try {
+			const result = await api.getResidentPieChartData(selectedResident.id);
+			console.log("result", result);
+			setData(result);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
 	return (
 		<Container>
 			<PageHeading>Pie Chart</PageHeading>
-			<ResidentName>{selectedResident?.firstName || "John Doe"}</ResidentName>
-			<MyResponsivePie />
+			<FormContainer>
+				<div>
+					<InputLabel>Select Resident</InputLabel>
+					<Select
+						options={residentsData}
+						// defaultValue={selectedResident}
+						onChange={setSelectedResident}
+						styles={selectOptionStyles}
+					/>
+				</div>
+				<ButtonContainer>
+					<SubmitButton onClick={viewResident}>View</SubmitButton>
+				</ButtonContainer>
+			</FormContainer>
+			<ResidentName>John Doe</ResidentName>
+			<MyResponsivePie data={data} />
 		</Container>
 	);
 };
